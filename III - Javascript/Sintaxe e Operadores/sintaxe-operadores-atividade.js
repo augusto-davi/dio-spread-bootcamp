eae4e879aba79b659560a9d28e10ec5554924071
Davi <obrigatoriopcional@gmail.com>
2022-05-01 19:19:44 -0300

    codigo refatorado

--- III - Javascript/Sintaxe e Operadores/sintaxe-operadores-atividade.js
+++ III - Javascript/Sintaxe e Operadores/sintaxe-operadores-atividade.js
@@ -1,22 +1,31 @@
-let checkEqualityValues, sum, sumString, compareSum, output;
-
 function numbers(x, y) {
-  checkEqualityValues =
+  const output = firstSentence(x, y) + secondSentence(x, y);
+
+  return output;
+}
+
+function firstSentence(x, y) {
+  const checkValues =
     x === y
       ? `Os números ${x} e ${y} são iguais. `
       : `Os números ${x} e ${y} não são iguais. `;
 
-  sum = x + y;
-  sumString = `Sua soma é igual a ${sum}, `;
+  return checkValues;
+}
+
+function secondSentence(x, y) {
+  const sum = x + y;
+  const sumString = `Sua soma é igual a ${sum}, `;
+  let compareSum;
 
   if (sum > 10 && sum < 20) compareSum = `que é maior que 10 e menor que 20.`;
   else if (sum < 10) compareSum = `que é menor que 10 e menor que 20.`;
   else if (sum > 20) compareSum = `que é maior que 10 e maior que 20.`;
   else compareSum = "Não pude comparar o valor da soma com 10 e 20.";
 
-  output = checkEqualityValues + sumString + compareSum;
+  const defineCompareSum = sumString + compareSum;
 
-  return output;
+  return defineCompareSum;
 }
 
-console.log(numbers(1, 1));
+console.log(numbers(1, 18));
